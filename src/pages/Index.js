0e726c2eb3488626ee562bcd5abdd889cd0aa6b0
Ari Pribadi <42528766+chorzkie@users.noreply.github.com>
2020-04-02 15:42:06 +0300

    Update src/pages/Index.js
    Co-Authored-By: faultable <farizrizaldy@icloud.com>

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -26,33 +26,3 @@ const index = () => {
 }
 
 export default index
-
-// import fetcher from "~/libs/fetch"
-// import useSWR from "swr";
-// import Layout from "../components/MyLayout"
-// import Post from "../components/Post"
-
-// const index = (props) => {
-
-//   const { data } = useSWR("/api/feed", fetcher);
-
-//   return (
-//     <Layout>
-//       <div className='to-render'>
-//         {data.map((eachEntry) => (
-//           <Post key={eachEntry.id} {...eachEntry} />
-//         ))}
-//       </div>
-
-//       <style jsx>
-//         {`
-//           .to-render {
-//             margin-top: 80px;
-//           }
-//         `}
-//       </style>
-//     </Layout>
-//   )
-// }
-
-// export default index
