8ff6a1ed7e22b1b23400a6ce545789648a2f60f5
Ari <chorzkie@gmail.com>
2020-04-02 17:32:08 +0300

    add_useSWR_in_Header.js

--- src/pages/users/[name].js
+++ src/pages/users/[name].js
@@ -1,28 +1,24 @@
-import Layout from '../../components/MyLayout'
-import Profile from '../../components/Profile'
-import fetch from 'isomorphic-unfetch'
+import Layout from '~/components/MyLayout'
+import Profile from '~/components/Profile'
+import fetcher from '~/libs/fetch'
+import useSWR from 'swr'
 import { useRouter } from 'next/router'
 
 const ProfilePage = (props) => {
   const router = useRouter()
-  const profileToLoad = props.profiles.filter((profile) => {
-    return profile.author.name.toLowerCase().includes(router.query.name)
-  })
+  const { data } = useSWR('/api/feed', fetcher)
 
   return (
     <Layout>
-      <Profile profileToLoad={profileToLoad} />
+      {data ? (
+        <Profile
+          profileToLoad={data.filter((profile) => {
+            return profile.author.name.toLowerCase().includes(router.query.name)
+          })}
+        />
+      ) : null}
     </Layout>
   )
-}
-
-ProfilePage.getInitialProps = async () => {
-  const res = await fetch('https://alchemy.edgy-network.workers.dev')
-  const data = await res.json()
-
-  return {
-    profiles: data,
-  }
 }
 
 export default ProfilePage
